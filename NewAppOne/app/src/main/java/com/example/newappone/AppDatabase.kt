b8c9cb765e82e2b75338bdfad33962c4627e0905
LHussell <46260172+LHussell@users.noreply.github.com>
2019-11-04 14:23:15 +0000

    Update AppDatabase.kt

--- NewAppOne/app/src/main/java/com/example/newappone/AppDatabase.kt
+++ NewAppOne/app/src/main/java/com/example/newappone/AppDatabase.kt
@@ -1,12 +1,13 @@
 package com.example.newappone
 
 import android.content.Context
-import androidx.room.*
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
 
 
 
 @Database(entities = [Notes::class], version = 1)
-@TypeConverters(TypeConverter::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun notesDao(): NotesDao
 
